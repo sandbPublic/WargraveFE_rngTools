28dbbeea0848848fabc315b248ae8583a4bd0822
Devon Sandberg <sandb146@umn.edu>
2020-04-25 11:42:21 -0500

    reorganized rns

--- feRandomNumbers.lua
+++ feRandomNumbers.lua
@@ -25,12 +25,12 @@ end
 
 local function bytesToPercent(rn)
 	if (GAME_VERSION == 6) then
-		-- rounds differently in FE6, simply divides by 655?
+		-- rounds differently in FE6, simply divides by 655
 		-- https://www.gamefaqs.com/boards/468480-fire-emblem/58065405?page=1
 		-- compare rn at 693 in FE6: raw is 0xB333, ~69.9996948242
 		-- but doesn't proc Bartre's 70 hp growth
 		-- 0xB333/655 ~70.0381679389
-		-- can produce 100s, if >= 0xFFDC (which is exactly 100)
+		-- can produce 100s, if >= 0xFFDC (which is exactly 100*655)
 		-- there are 2 fairly close together, at 670 and 688
 		return math.floor(rn/655)
 	else
@@ -38,38 +38,13 @@ local function bytesToPercent(rn)
 	end
 end
 
+
+
+
+
 local rnStreamObj = {}
 
-function rnStreamObj:new(rngMemoryOffset, primary)
-	local o = {} -- use numerical indexes as numbers in the stream
-	setmetatable(o, self)
-	self.__index = self
-	
-	o.rawBytes = {} -- save these in memory to find or move to correct position
-	o.strings = {} -- only for primary
-	o.rngAddr = rngMemoryOffset
-	o.isPrimary = primary
-	
-	if o.isPrimary then
-		o.rawBytes[-3] = 0x1496
-		o.rawBytes[-2] = 0x90EA
-		o.rawBytes[-1] = 0x3671
-	else
-		o.rawBytes[-1] = 0x3C7CA4D2
-	end
-	
-	o.pos = 0 -- position in the rn stream relative to gba power on
-	o.prevPos = 0
-	o.rnsGenerated = 0
-	-- # length operator is logarithmic, and indexing from -3 is more natural	
-	return o
-end
-
-P.rng1 = rnStreamObj:new(0x03000000, true)
--- secondary RN located at 0x03000008 in FE7
--- advanced by desert digs, blinking (suspend menu), 
--- and most usefully by merely opening a unit's item
-P.rng2 = rnStreamObj:new(0x03000008, false)
+-- non modifying functions
 
 function rnStreamObj:name()
 	if self.isPrimary then return "primary" end
@@ -77,8 +52,8 @@ function rnStreamObj:name()
 end
 
 -- gets rns that construct the next rn, initially the seed
-function rnStreamObj:generator(relative_prior_index)
-	return memory.readword(self.rngAddr + 2*(relative_prior_index - 1))
+function rnStreamObj:generator(relativePriorIndex)
+	return memory.readword(self.rngAddr + 2*(relativePriorIndex - 1))
 end
 
 -- generates more if needed
@@ -113,104 +88,17 @@ function rnStreamObj:getRN(pos_i, isRaw)
 	return self[pos_i]
 end
 
-function rnStreamObj:moveRNpos(delta)
-	local destination = self.pos + delta
-	if destination < 0 then destination = 0 end
-	
-	gen = self:getRN(destination-1, "isRaw")
-	
-	if self.isPrimary then
-		memory.writeword(self.rngAddr, gen)
-		memory.writeword(self.rngAddr+2, self.rawBytes[destination-2])
-		memory.writeword(self.rngAddr+4, self.rawBytes[destination-3])
-	else
-		memory.writeword(self.rngAddr, AND(gen, 0x0000FFFF))
-		memory.writeword(self.rngAddr+2, AND(gen, 0xFFFF0000)/0x10000)
-	end
-	
-	self:update()
-end
-
 -- returns false if current generators don't match previous 3 rns
 function rnStreamObj:isAtCurrentPosition()
 	local gen = self:getRN(self.pos-1, "isRaw")
 
 	return (self.isPrimary and 
-			(self.rawBytes[self.pos-3] == self:generator(3) and  -- other bytes are generated
+			(self.rawBytes[self.pos-3] == self:generator(3) and -- earlier raw bytes are already generated
 		     self.rawBytes[self.pos-2] == self:generator(2) and
 		     gen == self:generator(1)))
 			or
 			((not self.isPrimary) and
 			 gen == self:generator(1)+self:generator(2)*0x10000)
-end
-
-local lastFrameUpdated = 0
--- returns true if updated
-function rnStreamObj:update()
-	if not self:isAtCurrentPosition() then
-		self.prevPos = self.pos
-		self.pos = 0
-		while not self:isAtCurrentPosition() do
-			self.pos = self.pos + 1
-
-			-- sometimes the place in memory that holds the rns
-			-- temporarily holds other values
-			-- failsafe against this
-			
-			if self.pos % 10000 == 0 then
-				emu.frameadvance()
-			end
-			
-			if self.pos > MAX_RNS then
-				print(string.format(
-					"%s generators not found in rnStream within %d rns." 
-					.. " Skipping frame %d. Generators %04X %04X %04X", 
-					self:name(), MAX_RNS, vba.framecount(), 
-					self:generator(3), self:generator(2), self:generator(1)))
-				
-				self.pos = 0
-				emu.frameadvance()
-			end
-		end
-		
-		-- prevent 2ndary from printing a lot during crits
-		--if (not self.isPrimary) and (vba.framecount() - lastFrameUpdated) <= 4 then
-		--	lastFrameUpdated = vba.framecount()
-		--	return
-		--end
-		--lastFrameUpdated = vba.framecount()
-		
-		local rnPosDelta = self.pos - self.prevPos
-		
-		local str = string.format("RNG position %4d -> %4d, %+d", self.prevPos, self.pos, rnPosDelta)
-		if not self.isPrimary then
-			print("2ndary " .. str)
-			local str2 = "Next"
-			self:getRN(self.pos + WINDOW_WIDTH - 4)
-			for rn2_i = self.pos, self.pos + WINDOW_WIDTH - 4 do
-				if self[rn2_i] then
-					str2 = str2 .. "!"
-				else
-					str2 = str2 .. "."
-				end
-			end
-			print(str2)
-			return true
-		end
-		
-		-- print what was consumed if not a large jump
-		if rnPosDelta == 1 then -- print single rns on same line
-			print(self.strings[self.pos - 1] .. " " .. str)
-		elseif 0 < rnPosDelta and rnPosDelta <= 2*WINDOW_WIDTH/3 then -- print on up to 2 lines, need 3 chars per rn
-			print(self:rnSeqString(self.pos-rnPosDelta, rnPosDelta))
-			print("   " .. str)
-		else
-			print("   " .. str)
-		end
-		
-		return true
-	end
-	return false -- no update performed or needed
 end
 
 function rnStreamObj:printRawBytes(relativeStart, relativeEnd)
@@ -257,5 +145,120 @@ function rnStreamObj:RNstream_strings(isColored, numLines, rnsPerLine)
 	end
 	return rStrings
 end
+
+
+
+
+-- modifying functions
+
+-- returns true if updated
+function rnStreamObj:update()
+	if not self:isAtCurrentPosition() then
+		self.prevPos = self.pos
+		self.pos = 0
+		while not self:isAtCurrentPosition() do
+			self.pos = self.pos + 1
+
+			-- sometimes the place in memory that holds the rns
+			-- temporarily holds other values
+			-- failsafe against this
+			
+			if self.pos % 10000 == 0 then
+				emu.frameadvance()
+			end
+			
+			if self.pos > MAX_RNS then
+				print(string.format(
+					"%s generators not found in rnStream within %d rns." 
+					.. " Skipping frame %d. Generators %04X %04X %04X", 
+					self:name(), MAX_RNS, vba.framecount(), 
+					self:generator(3), self:generator(2), self:generator(1)))
+				
+				self.pos = 0
+				emu.frameadvance()
+			end
+		end
+				
+		local rnPosDelta = self.pos - self.prevPos
+		
+		local str = string.format("RNG position %4d -> %4d, %+d", self.prevPos, self.pos, rnPosDelta)
+		if not self.isPrimary then
+			print("2ndary " .. str)
+			local str2 = "Next"
+			self:getRN(self.pos + WINDOW_WIDTH - 4)
+			for rn2_i = self.pos, self.pos + WINDOW_WIDTH - 4 do
+				if self[rn2_i] then
+					str2 = str2 .. "!"
+				else
+					str2 = str2 .. "."
+				end
+			end
+			print(str2)
+			return true
+		end
+		
+		-- print what was consumed if not a large jump
+		if rnPosDelta == 1 then -- print single rns on same line
+			print(self.strings[self.pos - 1] .. " " .. str)
+		elseif 0 < rnPosDelta and rnPosDelta <= 2*WINDOW_WIDTH/3 then -- print on up to 2 lines, need 3 chars per rn
+			print(self:rnSeqString(self.pos-rnPosDelta, rnPosDelta))
+			print("   " .. str)
+		else
+			print("   " .. str)
+		end
+		
+		return true
+	end
+	return false -- no update performed or needed
+end
+
+function rnStreamObj:moveRNpos(delta)
+	local destination = self.pos + delta
+	if destination < 0 then destination = 0 end
+	
+	gen = self:getRN(destination-1, "isRaw")
+	
+	if self.isPrimary then
+		memory.writeword(self.rngAddr, gen)
+		memory.writeword(self.rngAddr+2, self.rawBytes[destination-2])
+		memory.writeword(self.rngAddr+4, self.rawBytes[destination-3])
+	else
+		memory.writeword(self.rngAddr, AND(gen, 0x0000FFFF))
+		memory.writeword(self.rngAddr+2, AND(gen, 0xFFFF0000)/0x10000)
+	end
+	
+	self:update()
+end
+
+function rnStreamObj:new(rngMemoryOffset, primary)
+	local o = {} -- use numerical indexes as numbers in the stream
+	setmetatable(o, self)
+	self.__index = self
+	
+	o.rawBytes = {} -- save these in memory to find or move to correct position
+	o.strings = {} -- only for primary
+	o.rngAddr = rngMemoryOffset
+	o.isPrimary = primary
+	
+	if o.isPrimary then
+		o.rawBytes[-3] = 0x1496
+		o.rawBytes[-2] = 0x90EA
+		o.rawBytes[-1] = 0x3671
+	else
+		o.rawBytes[-1] = 0x3C7CA4D2
+	end
+	
+	o.pos = 0 -- position in the rn stream relative to gba power on
+	o.prevPos = 0
+	o.rnsGenerated = 0
+	-- # length operator is logarithmic, and indexing from -3 is more natural	
+	return o
+end
+
+P.rng1 = rnStreamObj:new(0x03000000, true)
+-- secondary RN located at 0x03000008 in FE6/7
+-- advanced by desert digs, blinking (suspend menu), 
+-- and most usefully by merely opening a unit's items
+P.rng2 = rnStreamObj:new(0x03000008, false)
 
 return rns
